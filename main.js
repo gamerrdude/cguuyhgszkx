e48b4d0a2a794bacd7f482aa09577f86fe3597c9
gamerrdude <87709109+gamerrdude@users.noreply.github.com>
2022-01-14 20:02:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload(){
+    img = loadImage('dog_cat.jpg');
+    status = "";
+}
+
+function setup(){
+canvas = createCanvas(640, 420);
+canvas.center();
+objectdetector = ml5.objectdetector('cocossed', modelLoaded);
+document.getElementById("status").innerHTML = "Status: detecting object";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("doggy", 45, 75);
+    nofill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("CAT", 320, 120);
+    noFill()
+    stroke("#FF0000")
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+    status = true;
+    objectdetector.detect(img, gotResult);
+
+}
+
+function gotResult(error, Results){
+if(error){
+    console.log(error);
+
+}
+console.log(results);
+}
